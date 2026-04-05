33cca4034058332d57df608db146b734400e5ce0
rehan0018 <rehanwshaikh918@gmail.com>
2026-04-05 21:41:54 +0530

    Final Commit: Hero Image, Report Gen, Code Polish

--- users/views.py
+++ users/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
+from .forms import CustomUserCreationForm
 
 def register_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             messages.success(request, 'Registration successful.')
             return redirect('products:product_list')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
 
 def login_view(request):
